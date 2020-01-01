f90c84105eec8896c62b36a99f1be414034b6c3c
rachel.wootton <racheljanewootton@gmail.com>
2020-01-01 15:35:35 -0700

    ding flowers

--- src/common/Tulip.js
+++ src/common/Tulip.js
@@ -2,19 +2,9 @@ import React, { useEffect} from 'react';
 import Entity from './Entity';
 
 const Tulip = (props) => {
-  const { scene, interactMap } = props;
-
-  useEffect(()=>{
-      if(scene && interactMap) {
-        const {x, y, z} = props.position;
-        const {id} = props;
-        interactMap.add({x, y, z, radius: 200, id});
-      }
-  }, [scene, interactMap])
 
   return <Entity 
     scale={15}
-    radius={20}
     file={'assets/tulip.glb'}
     castShadow={false}
     {...props}
